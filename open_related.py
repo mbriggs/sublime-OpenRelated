eeb85ce55f560104f7da931b77dc011a397bbab1
Matt Briggs <matt@mattbriggs.net>
2013-01-01 12:49:30 -0500

    choose when there are multiple possibilities
    if there is one possibility, go to it. if there are many, prompt for the choice

--- open_related.py
+++ open_related.py
@@ -1,24 +1,58 @@
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
 import os.path
 import converter
 
+
+class RelatedFiles(object):
+    window = None
+    paths = None
+    root = None
+
+    def __init__(self, window):
+        self.window = window
+        self.paths = []
+        self.root = window.folders()[0]
+
+    def append(self, path):
+        self.paths.append(path)
+
+    def choose_path(self):
+        if len(self.paths) > 1:
+            choices = [path.replace(self.root + '/', '') for path in self.paths]
+            self.window.show_quick_panel(choices, self.open_path)
+        elif len(self.paths) == 1:
+            self.open_path(0)
+        else:
+            sublime.status_message("Cannot find related file !")
+
+    def open_path(self, index):
+        if index == -1:
+            return
+
+        win = self.window
+        path = self.paths[index]
+
+        if win.num_groups() > 1:
+            win.focus_group((win.active_group() + 1) % win.num_groups())
+
+        win.open_file(path)
+
+
 class OpenRelatedCommand(sublime_plugin.WindowCommand):
     def run(self):
-        win = self.window
-        view = win.active_view()
+        related_files = RelatedFiles(self.window)
+        view = self.window.active_view()
         current_file = view.file_name()
-        found = False
 
         for patterns in view.settings().get('open_related_patterns', []):
-            for file in converter.create(patterns, sublime.platform()).convert(current_file):
-                if os.path.exists(file):
-                    if win.num_groups() > 1:
-                        win.focus_group((win.active_group() + 1) % win.num_groups())
-                    self.window.open_file(file)
-                    found = True
-            if found: return
+            paths = converter.create(patterns, sublime.platform()).convert(current_file)
 
-        sublime.status_message("Cannot find related file !")
+            for path in paths:
+                if os.path.exists(path):
+                    related_files.append(path)
+
+        related_files.choose_path()
 
     def is_enabled(self):
         return self.window.active_view() != None
